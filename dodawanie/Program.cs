17d7b0674f98341940d2a1821837b864c3cc0fc2
Noraz69 <41554263+Noraz69@users.noreply.github.com>
2019-01-15 23:02:09 +0100

    lekko poprawione i dziala

--- dodawanie/Program.cs
+++ dodawanie/Program.cs
@@ -14,30 +14,34 @@ namespace dodawanie
             String SecondNumber = "";
             int Sum;
             Console.WriteLine("Witamy w dodwaniu");
+            Start:
             Console.WriteLine("Podaj Pierwsza Liczbe 0-10: ");
-            FirstNumber=Console.ReadLine();
+            
+            FirstNumber =Console.ReadLine();
             int FirstNum = int.Parse(FirstNumber);
 
-            while(true)
-            if (FirstNum > 10 || FirstNum < 0)
-            {
-                Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
-                break;
+             if (FirstNum > 10 || FirstNum < 0)
+                {
+                    Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
+                    goto Start;
+                }
 
-            }
-            
+             Start2:
+
             Console.WriteLine("Podaj druga liczbe 0-10: ");
             SecondNumber=Console.ReadLine();
             int SecondNum = int.Parse(SecondNumber);
-            while (true)
+            
                 if (SecondNum > 10 || SecondNum < 0)
                 {
                     Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
-                    break;
+                    goto Start2;
                 }
+            
+                
             Sum = FirstNum + SecondNum;
-            Console.WriteLine("Wynik dodawania to :" + Sum + "");
-
+            Console.WriteLine("Wynik dodawania to : " + Sum + "");
+            Console.ReadKey();
         }
     }
 }
