60e71bb924fecd9e79b90f041e6a304604f472db
Noraz69 <41554263+Noraz69@users.noreply.github.com>
2019-01-16 11:08:28 +0100

    nie dzialajaca petla

--- dodawanie/Program.cs
+++ dodawanie/Program.cs
@@ -15,81 +15,96 @@ namespace dodawanie
             int Sum;
             int FirstNum = 0;
             int SecondNum = 0;
-
+            String con = "";
             Console.WriteLine("Witamy w dodwaniu");
 
-        Start:
-            try
+            do
             {
-                Console.WriteLine("Podaj Pierwsza Liczbe 0-10: ");
-                FirstNumber = Console.ReadLine();
-                FirstNum = int.Parse(FirstNumber);
-
-
-
-                if (FirstNum > 10 || FirstNum < 0)
+                try
                 {
-                    Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
-                    goto Start;
+                    Console.WriteLine("Podaj Pierwsza Liczbe 0-10: ");
+                    FirstNumber = Console.ReadLine();
+                    FirstNum = int.Parse(FirstNumber);
+
+
+
+                    if (FirstNum > 10 || FirstNum < 0)
+                    {
+                        Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
+
+                    }
+                    else
+                        con = "wkoncu";
+
                 }
-            }
-            catch (FormatException fEx)
-            {
-                Console.WriteLine(fEx.Message);
-                goto Start;
-            }
-            catch (OverflowException OverEx)
-            {
-                Console.WriteLine(OverEx.Message);
-                goto Start;
-            }
-            catch (ArithmeticException ArgEx)
-            {
-                Console.WriteLine(ArgEx.Message);
-                goto Start;
-            }
-            catch (Exception Ex)
-            {
-                Console.WriteLine("Coœ posz³o nie tak");
-                goto Start;
-            }
-
-        Start2:
-
-            try
-            {
-                Console.WriteLine("Podaj druga liczbe 0-10: ");
-                SecondNumber = Console.ReadLine();
-                SecondNum = int.Parse(SecondNumber);
-
-                if (SecondNum > 10 || SecondNum < 0)
+                catch (FormatException fEx)
                 {
-                    Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
-                    goto Start2;
-                }
-            }
-            catch (FormatException fEx)
-            {
-                Console.WriteLine(fEx.Message);
-                goto Start2;
-            }
-            catch (OverflowException OverEx)
-            {
-                Console.WriteLine(OverEx.Message);
-                goto Start2;
-            }
-            catch (ArithmeticException ArgEx)
-            {
-                Console.WriteLine(ArgEx.Message);
-                goto Start2;
-            }
-            catch (Exception Ex)
-            {
-                Console.WriteLine("Coœ posz³o nie tak");
-                goto Start2;
-            }
+                    Console.WriteLine(fEx.Message);
 
-            
+
+                }
+                catch (OverflowException OverEx)
+                {
+                    Console.WriteLine(OverEx.Message);
+
+
+                }
+                catch (ArithmeticException ArgEx)
+                {
+                    Console.WriteLine(ArgEx.Message);
+
+
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine("Coœ posz³o nie tak");
+
+
+                }
+            } while (con == "wkoncu");
+
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Podaj druga liczbe 0-10: ");
+                    SecondNumber = Console.ReadLine();
+                    SecondNum = int.Parse(SecondNumber);
+
+                    if (SecondNum > 10 || SecondNum < 0)
+                    {
+                        Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
+
+                    }
+                    else
+                        con = "wkoncu";
+
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine(fEx.Message);
+
+                }
+                catch (OverflowException OverEx)
+                {
+                    Console.WriteLine(OverEx.Message);
+
+                }
+                catch (ArithmeticException ArgEx)
+                {
+                    Console.WriteLine(ArgEx.Message);
+
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine("Coœ posz³o nie tak");
+
+                }
+            } while (con == "wkoncu");
+
+
+
             Sum = FirstNum + SecondNum;
             Console.WriteLine("Wynik dodawania to : " + Sum + "");
             Console.ReadKey();
